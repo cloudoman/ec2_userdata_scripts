56ba11f015d11589df9ccf3b651e80240a060546
Ameer Deen <ameer.deen@servicemesh.com>
2012-12-12 09:33:02 +1100

    Added License

--- install_windows_agent.ps1
+++ install_windows_agent.ps1
@@ -1,4 +1,26 @@
 # ------------------------------------------------------------------------------------------
+# License and Authors
+# ===================
+#
+# Author:: Ameer Deen (ameer.deen@cloudoman.com)
+#
+# Copyright:: 2011-2012, Cloudoman PTY LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------------------
 # This is a powershell userdata script used to bootstrap Amazon's Windows AMI's with 
 # RightScale's server agent called RightLink (https://github.com/rightscale/right_link)
 # ------------------------------------------------------------------------------------------
